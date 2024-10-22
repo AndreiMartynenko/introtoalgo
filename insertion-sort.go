991b6179ff987352ce7f7f4c6a7a2488dd0fb5e9
AndrewLondon <andrewmart@mail.com>
2024-10-22 13:03:59 +0100

    Insertion sort

--- insertion-sort.go
+++ insertion-sort.go
@@ -2,19 +2,37 @@ package main
 
 import "fmt"
 
+// non-decreasing order
+//func insertionSort(arr []int) []int {
+//	// Implement insertion sort here
+//	for j := 1; j < len(arr); j++ {
+//		key := arr[j]
+//		i := j - 1
+//		for i >= 0 && arr[i] > key {
+//			arr[i+1] = arr[i]
+//			i = i - 1
+//		}
+//		arr[i+1] = key
+//	}
+//	return arr
+//
+//}
+
+// non-increasing order
+
 func insertionSort(arr []int) []int {
 	// Implement insertion sort here
 	for j := 1; j < len(arr); j++ {
+		//fmt.Println(j)
 		key := arr[j]
 		i := j - 1
-		for i >= 0 && arr[i] > key {
+		for i >= 0 && arr[i] < key {
 			arr[i+1] = arr[i]
 			i = i - 1
 		}
 		arr[i+1] = key
 	}
 	return arr
-
 }
 
 func main() {
